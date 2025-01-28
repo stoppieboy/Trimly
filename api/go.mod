e4b9a441a4d0aee8b9f065f6983bf8da1dd2665f
Shivam Verma <shivamv1332@gmail.com>
2025-01-28 21:04:57 +0530

    database abstraction for redis

--- api/go.mod
+++ api/go.mod
@@ -3,12 +3,16 @@ module github.com/stoppieboy/trimly
 go 1.23.4
 
 require (
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2
 	github.com/gofiber/fiber/v2 v2.52.6
 	github.com/joho/godotenv v1.5.1
+	github.com/redis/go-redis/v9 v9.7.0
 )
 
 require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -3,7 +3,9 @@ package routes
 import (
 	"time"
 
+	"github.com/asaskevich/govalidator"
 	"github.com/gofiber/fiber/v2"
+	"github.com/stoppieboy/trimly/helpers"
 )
 
 type Request struct {
@@ -20,6 +22,26 @@ type Response struct {
 	XRateLimitReset     time.Duration `json:"rate_limit_reset"`
 }
 
-func ShortenURL(c *fiber.Ctx) Response {
-	return Response{}
+func ShortenURL(c *fiber.Ctx) error {
+	body := new(Request)
+
+	if err := c.BodyParser(body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "cannot parse JSON"})
+	}
+
+	// implement rate limiting
+	// check if the input is an actual URL
+	if !govalidator.IsURL(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid URL"})
+	}
+
+	// check for domain error
+	if !helpers.RemoveDomainError(body.URL){
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error": "Domain error"})
+	}
+
+	// enforce https, SSL
+	body.URL = helpers.EnforceHTTPS(body.URL)
+
+	return nil
 }
