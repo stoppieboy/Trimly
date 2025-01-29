a9448a9fab53785f1af7a86f7d08a7a6059887f9
Shivam Verma <shivamv1332@gmail.com>
2025-01-30 00:15:03 +0530

    shorten patched

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -21,11 +21,11 @@ type Request struct {
 }
 
 type Response struct {
-	URL                 string        `json:"url"`
-	CustomShort         string        `json:"short"`
-	Expiry              time.Duration `json:"expiry"`
-	XRateLimitRemaining int           `json:"rate_limit"`
-	XRateLimitReset     time.Duration `json:"rate_limit_reset"`
+	URL             string        `json:"url"`
+	CustomShort     string        `json:"short"`
+	Expiry          time.Duration `json:"expiry"`
+	XRateRemaining  int           `json:"rate_limit"`
+	XRateLimitReset time.Duration `json:"rate_limit_reset"`
 }
 
 func ShortenURL(c *fiber.Ctx) error {
@@ -34,6 +34,9 @@ func ShortenURL(c *fiber.Ctx) error {
 	if err := c.BodyParser(body); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "cannot parse JSON"})
 	}
+
+	r := database.CreateClient(0)
+	defer r.Close()
 
 	// implement rate limiting
 
@@ -44,14 +47,14 @@ func ShortenURL(c *fiber.Ctx) error {
 	value, err := r2.Get(database.Ctx, c.IP()).Result()
 	if err == redis.Nil {
 		_ = r2.Set(database.Ctx, c.IP(), os.Getenv("API_QUOTA"), 30*60*time.Second).Err()
-	}else if err != nil {
+	} else if err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Some error"})
-	}else {
+	} else {
 		valInt, _ := strconv.Atoi(value)
 		if valInt <= 0 {
 			limit, _ := r2.TTL(database.Ctx, c.IP()).Result()
 			return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
-				"error": "Rate limit exceeded",
+				"error":            "Rate limit exceeded",
 				"rate_limit_reset": limit / time.Nanosecond / time.Minute,
 			})
 		}
@@ -63,13 +66,12 @@ func ShortenURL(c *fiber.Ctx) error {
 	}
 
 	// check for domain error
-	if !helpers.RemoveDomainError(body.URL){
+	if !helpers.RemoveDomainError(body.URL) {
 		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error": "Domain error"})
 	}
 
 	// enforce https, SSL
 	body.URL = helpers.EnforceHTTPS(body.URL)
-
 
 	var id string
 
@@ -81,7 +83,7 @@ func ShortenURL(c *fiber.Ctx) error {
 	}
 
 	// check if the custom short is already taken
-	if checkShortTaken(id) {
+	if checkShortTaken(r, id) {
 		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{"error": "Custom short already taken"})
 	}
 
@@ -90,20 +92,24 @@ func ShortenURL(c *fiber.Ctx) error {
 		body.Expiry = 24
 	}
 
-	err = r2.Set(database.Ctx, id, body.URL, body.Expiry*3600*time.Second).Err()
+	err = r.Set(database.Ctx, id, body.URL, body.Expiry*3600*time.Second).Err()
 	if err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Unable to connect to server"})
 	}
-	fmt.Println(id)
+	resp := Response{
+		URL:             body.URL,
+		CustomShort:     os.Getenv("DOMAIN") + "/" + id,
+		Expiry:          body.Expiry,
+		XRateRemaining:  int(r2.TTL(database.Ctx, c.IP()).Val()),
+		XRateLimitReset: r2.TTL(database.Ctx, c.IP()).Val() / time.Nanosecond / time.Minute,
+	}
 
 	r2.Decr(database.Ctx, c.IP())
 
-	return nil
+	return c.Status(fiber.StatusCreated).JSON(resp)
 }
 
-func checkShortTaken(short string) bool {
-	r := database.CreateClient(0)
-	defer r.Close()
+func checkShortTaken(r *redis.Client,short string) bool {
 
 	val, _ := r.Get(database.Ctx, short).Result()
 	if val != "" {
